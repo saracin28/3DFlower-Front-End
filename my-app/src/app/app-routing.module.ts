0e236c1f4dc34aeebf021c46662e3de51910889b
saracin28 <robert.saracin@yahoo.com>
2020-07-01 15:23:05 +0300

    Routing created + HTML improved

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AccessoriesComponent} from "./components/accessories/accessories.component";
+import {FlowersComponent} from "./components/flowers/flowers.component";
+import {PotComponent} from "./components/pot/pot.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"accessories",component:AccessoriesComponent},
+  {path:"flowers",component:FlowersComponent},
+  {path:"pot",component:PotComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
