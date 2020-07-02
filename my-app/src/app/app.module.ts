f6c3b0e38f00ee558b74f6f245da46681846623d
saracin28 <robert.saracin@yahoo.com>
2020-07-02 20:58:07 +0300

    updated

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PotComponent } from './components/pot/pot.component';
 import { PotContentComponent } from './components/pot/pot-content/pot-content.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { OurProductsComponent } from './components/our-products/our-products.component';
+import { CartComponent } from './components/cart/cart.component';
+import { AllComponentsComponent } from './components/all-components/all-components.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PotComponent,
-    PotContentComponent
+    PotContentComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    OurProductsComponent,
+    CartComponent,
+    AllComponentsComponent
   ],
   imports: [
     BrowserModule,
