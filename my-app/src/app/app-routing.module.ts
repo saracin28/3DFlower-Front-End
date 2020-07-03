cdac6f91b154b17d59e3daa3e637e41cbbe55a4f
saracin28 <robert.saracin@yahoo.com>
2020-07-03 23:21:27 +0300

    Routes created

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import {AccessoriesComponent} from "./components/accessories/accessories.component";
 import {FlowersComponent} from "./components/flowers/flowers.component";
 import {PotComponent} from "./components/pot/pot.component";
+import {HomeComponent} from "./components/home/home.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {OurProductsComponent} from "./components/our-products/our-products.component";
+import {CartComponent} from "./components/cart/cart.component";
+import {LoginComponent} from "./components/login/login.component";
 
 
 const routes: Routes = [
   {path:"accessories",component:AccessoriesComponent},
   {path:"flowers",component:FlowersComponent},
   {path:"pot",component:PotComponent},
+  {path:"home",component:HomeComponent},
+  {path:"our-products",component:OurProductsComponent},
+  {path:"cart",component:CartComponent},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
 ];
 
 @NgModule({
