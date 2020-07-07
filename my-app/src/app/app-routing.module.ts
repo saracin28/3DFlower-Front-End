489068cdf74e62dea63cfb79a7887a0ca68f0259
RoxanaCantemir <roxana_c1991@yahoo.com>
2020-07-07 17:14:13 +0300

    login

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,27 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AccessoriesComponent} from "./components/accessories/accessories.component";
-import {FlowersComponent} from "./components/flowers/flowers.component";
-import {PotComponent} from "./components/pot/pot.component";
-import {HomeComponent} from "./components/home/home.component";
-import {RegisterComponent} from "./components/register/register.component";
-import {OurProductsComponent} from "./components/our-products/our-products.component";
-import {CartComponent} from "./components/cart/cart.component";
-import {LoginComponent} from "./components/login/login.component";
-import {PotSummaryComponent} from "./components/pot/pot-content/pot-summary/pot-summary.component";
-
+import {AccessoriesComponent} from './components/accessories/accessories.component';
+import {FlowersComponent} from './components/flowers/flowers.component';
+import {PotComponent} from './components/pot/pot.component';
+import {HomeComponent} from './components/home/home.component';
+import {RegisterComponent} from './components/register/register.component';
+import {OurProductsComponent} from './components/our-products/our-products.component';
+import {CartComponent} from './components/cart/cart.component';
+import {LoginComponent} from './components/login/login.component';
+import {PotSummaryComponent} from './components/pot/pot-content/pot-summary/pot-summary.component';
+import {UserComponent} from './components/user/user.component';
 
 const routes: Routes = [
-  {path:"accessories",component:AccessoriesComponent},
-  {path:"flowers",component:FlowersComponent},
-  {path:"pot",component:PotComponent},
-  {path:"home",component:HomeComponent},
-  {path:"our-products",component:OurProductsComponent},
-  {path:"cart",component:CartComponent},
-  {path:"login",component:LoginComponent},
-  {path:"register",component:RegisterComponent},
-  {path:"pot/pot-summary",component:PotSummaryComponent},
-  {path:"",component:HomeComponent},
+  {path: 'accessories', component: AccessoriesComponent},
+  {path: 'flowers', component: FlowersComponent},
+  {path: 'pot', component: PotComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'our-products', component: OurProductsComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'pot/pot-summary', component: PotSummaryComponent},
+  {path: '', component: HomeComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', component: LoginComponent}
 ];
 
 @NgModule({
---
+++ my-app/src/app/material.module.ts
@@ -0,0 +1,40 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatDialogModule,
+    MatTableModule,
+    MatMenuModule,
+    MatIconModule,
+    MatProgressSpinnerModule
+  ],
+  exports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatDialogModule,
+    MatTableModule,
+    MatMenuModule,
+    MatIconModule,
+    MatProgressSpinnerModule
+  ],
+})
+export class CustomMaterialModule {
+}
