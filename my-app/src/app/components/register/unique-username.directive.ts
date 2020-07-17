55266018f361d21013974b587d0004d7cd031f47
saracin28 <robert.saracin@yahoo.com>
2020-07-17 23:25:04 +0300

    Register done for good

--- my-app/src/app/components/register/unique-username.directive.ts
+++ my-app/src/app/components/register/unique-username.directive.ts
@@ -5,18 +5,22 @@ import {HttpServiceService} from "../../services/http/http-service.service";
 import {map} from "rxjs/operators";
 
 @Directive({
-  selector: '[uniqueUsername]',
-  providers: [{provide: NG_ASYNC_VALIDATORS,
-  useExisting: UniqueUsernameDirective,
-  multi: true}]
+  selector: '[alreadyExist]',
+  providers: [{
+    provide: NG_ASYNC_VALIDATORS,
+    useExisting: UniqueUsernameDirective,
+    multi: true
+  }]
 })
-export class UniqueUsernameDirective implements AsyncValidator{
+
+export class UniqueUsernameDirective implements AsyncValidator {
   constructor(private httpService: HttpServiceService) {
   }
-  validate(c: AbstractControl): Promise<ValidationErrors | null > | Observable<ValidationErrors | null>{
+
+  validate(c: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
     return this.httpService.getUserByName(c.value).pipe(
       map(users => {
-        return users && users.length >0 ? {'uniqueUsername': true} :null;
+        return users  ? {'alreadyExist': true} : null;
       })
     )
   }
